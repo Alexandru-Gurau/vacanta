8f991aebf74a314b859b650ccf34fa0346f59ca8
Alexandru-Gurau <alexandru.gurau@live.com>
2021-11-03 14:18:24 +0200

    trip booking page v6

--- src/javascript/PageBooking.js
+++ src/javascript/PageBooking.js
@@ -6,3 +6,22 @@ import {
   buttonShoppingX,
 } from './Shopping/shopping';
 import { bookingContainer } from './Trips/tripsLayout';
+
+// BUTTON SEE MORE or LESS
+const btnSeeMore = document.getElementById('button-see-more');
+const btnSeeLess = document.getElementById('button-see-less');
+
+const showBookingContainer = () => {
+  bookingContainer.style.height = 'auto';
+  btnSeeMore.style.display = 'none';
+  btnSeeLess.style.display = 'block';
+};
+const hideBookingContainer = () => {
+  bookingContainer.style.height = '100rem';
+  btnSeeMore.style.display = 'block';
+  btnSeeLess.style.display = 'none';
+};
+btnSeeMore.addEventListener('click', showBookingContainer);
+btnSeeLess.addEventListener('click', hideBookingContainer);
+
+// /////////////////////////////////////////////////////////
