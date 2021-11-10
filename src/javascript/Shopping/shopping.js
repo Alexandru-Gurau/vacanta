d2d31dc307dc59260f4226c9d560ef51ab54a646
Alexandru-Gurau <alexandru.gurau@live.com>
2021-11-10 14:20:59 +0200

    trip booking page cards v2

--- src/javascript/Shopping/shopping.js
+++ src/javascript/Shopping/shopping.js
@@ -11,7 +11,7 @@ const headerMesage = document.querySelector('#header-message');
 
 const openShoppingBox = () => {
   dropdownMenu.classList.toggle('hide-dropdown');
-  headerMesage.innerHTML = 'Add some trips 😞';
+  headerMesage.innerHTML = 'Add some trips 🌎';
 };
 const closeShoppingBox = () => {
   dropdownMenu.classList.add('hide-dropdown');
