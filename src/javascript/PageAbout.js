2a8b5c42f79d140bcaa7b75f8f1706ca563bee14
Alexandru-Gurau <alexandru.gurau@live.com>
2021-11-14 20:57:46 +0200

    about page v12

--- src/javascript/PageAbout.js
+++ src/javascript/PageAbout.js
@@ -3,18 +3,21 @@ const expandCardText = () => {
   const aboutCardText = document.getElementsByClassName('about-story__text');
   const header = document.getElementsByClassName('headerJS');
   const paragraph = document.getElementsByClassName('paragraphJS');
+  const aboutStory = document.querySelector('.about-story');
 
   aboutCard.forEach((el, i) => {
     el.addEventListener('mouseover', function () {
       aboutCardText[i].style.width = '100%';
       header[i].style.display = 'none';
-      setTimeout(() => {
-        paragraph[i].style.display = 'flex';
-      }, 300);
+      paragraph[i].style.display = 'flex';
     });
     el.addEventListener('mouseout', function () {
       aboutCardText[i].style.width = '50%';
       header[i].style.display = 'block';
+      paragraph[i].style.display = 'none';
+    });
+
+    aboutStory.addEventListener('click', () => {
       paragraph[i].style.display = 'none';
     });
   });
