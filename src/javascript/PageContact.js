cb2abead6dbc28dcffcec801fa5a54b3170f7380
Alexandru-Gurau <alexandru.gurau@live.com>
2021-11-21 11:21:45 +0200

    contact page v7

--- src/javascript/PageContact.js
+++ src/javascript/PageContact.js
@@ -4,6 +4,15 @@ const headerMessage = document.getElementById('header-contact-message');
 const containerMessage = document.querySelector('.contact__message');
 const hiddenMessage = document.getElementById('messageHidden');
 
+const nameMessage = document.getElementById('name-message');
+const emailMessage = document.getElementById('email-message');
+const textareaMessage = document.getElementById('textarea-message'); //momentan .value nu functioneaza. Area val null
+
+const clearInputsMessage = () => {
+  nameMessage.value = '';
+  emailMessage.value = '';
+};
+
 const messageSend = () => {
   headerMessage.style.display = 'none';
   formMessage.style.display = 'none';
@@ -13,11 +22,14 @@ const init = () => {
   hiddenMessage.classList.add('message-hidden');
   headerMessage.style.display = 'block';
   formMessage.style.display = 'flex';
+  clearInputsMessage();
 };
 
 btnMessage.addEventListener('click', (e) => {
   e.preventDefault();
   messageSend();
+  console.log(nameMessage.value);
+  console.log(emailMessage.value);
   setTimeout(() => {
     init();
   }, 3000);
