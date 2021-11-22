0336e8427c68a53368d58cc776c19cc3362a1857
Alexandru-Gurau <alexandru.gurau@live.com>
2021-11-22 12:23:14 +0200

    contact page v8

--- src/javascript/PageContact.js
+++ src/javascript/PageContact.js
@@ -3,34 +3,70 @@ const formMessage = document.getElementById('form-contact-message');
 const headerMessage = document.getElementById('header-contact-message');
 const containerMessage = document.querySelector('.contact__message');
 const hiddenMessage = document.getElementById('messageHidden');
+const contactContainerMessage = document.getElementById(
+  'contact-message-container'
+);
 
 const nameMessage = document.getElementById('name-message');
 const emailMessage = document.getElementById('email-message');
-const textareaMessage = document.getElementById('textarea-message'); //momentan .value nu functioneaza. Area val null
+const textareaMessage = document.getElementById('myTextarea');
 
 const clearInputsMessage = () => {
   nameMessage.value = '';
   emailMessage.value = '';
+  textareaMessage.value = '';
 };
 
 const messageSend = () => {
   headerMessage.style.display = 'none';
   formMessage.style.display = 'none';
-  hiddenMessage.classList.remove('message-hidden');
+  contactContainerMessage.classList.remove('message-hidden');
+
+  if (
+    nameMessage.value == 0 ||
+    emailMessage.value == 0 ||
+    textareaMessage.value == 0
+  ) {
+    hiddenMessage.innerHTML = `
+  <h2
+              class="header header__contact text-color-tertiary"
+              id="messageHidden"
+            >
+              Please complete all inputs.
+            </h2>
+  `;
+    return;
+  } else {
+    hiddenMessage.innerHTML = `
+  <h2
+              class="header header__contact text-color-tertiary"
+              id="messageHidden"
+            >
+              Thank you for message.
+            </h2>
+  `;
+  }
 };
 const init = () => {
-  hiddenMessage.classList.add('message-hidden');
   headerMessage.style.display = 'block';
   formMessage.style.display = 'flex';
   clearInputsMessage();
+  contactContainerMessage.classList.add('message-hidden');
 };
 
 btnMessage.addEventListener('click', (e) => {
   e.preventDefault();
   messageSend();
-  console.log(nameMessage.value);
-  console.log(emailMessage.value);
   setTimeout(() => {
     init();
   }, 3000);
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    messageSend();
+    setTimeout(() => {
+      init();
+    }, 3000);
+  }
 });
